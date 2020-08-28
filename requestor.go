b063f2b7f9d0ebd1d9177af4a2deb1a12f10bc9a
vmanikes <vmanikes@hawk.iit.edu>
2020-08-27 22:42:36 -0600

    Completed Adding JSON support

--- requestor.go
+++ requestor.go
@@ -2,8 +2,11 @@
 package Requestor
 
 import (
+	"bytes"
 	"crypto/tls"
+	"encoding/json"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -50,6 +53,10 @@ func New() (client *Client) {
 	}
 }
 
+func (c *Client) SetTLSClientConfig(tlsConfig *tls.Config) {
+	c.TLSClientConfig = tlsConfig
+}
+
 func (c *Client) DisableKeepAlive(val bool) {
 	c.DisableKeepAlives = val
 }
@@ -82,74 +89,141 @@ func (c *Client) SetIdleConnectionTimeout(timeout time.Duration) {
 // error if exist
 func (c *Client) Get(url string, headers, queryParams map[string][]string) (response *http.Response, err error) {
 
-	return c.makeRequest(url, http.MethodGet, headers, nil, queryParams, nil)
+	return c.makeRequest(url, http.MethodGet, headers, queryParams, nil)
 }
 
 // Head performs a HTTP HEAD request. It takes in a URL, user specified headers, query params and returns Response and
 // error if exist
 func (c *Client) Head(url string, headers, queryParams map[string][]string) (response *http.Response, err error) {
 
-	return c.makeRequest(url, http.MethodHead, headers, nil, queryParams, nil)
+	return c.makeRequest(url, http.MethodHead, headers, queryParams, nil)
 }
 
 // Post performs a HTTP POST request. It takes in a URL, user specified headers, query params, data and returns
 // Response and error if exist
 func (c *Client) Post(url string, headers, queryParams map[string][]string, data interface{}) (response *http.Response, err error) {
 
-	return c.makeRequest(url, http.MethodPost, headers, nil, queryParams, data)
+	return c.makeRequest(url, http.MethodPost, headers, queryParams, data)
 }
 
 // Put performs a HTTP PUT request. It takes in a URL, user specified headers, query params, data and returns
 // Response and error if exist
 func (c *Client) Put(url string, headers, queryParams map[string][]string, data interface{}) (response *http.Response, err error) {
 
-	return c.makeRequest(url, http.MethodPut, headers, nil, queryParams, data)
+	return c.makeRequest(url, http.MethodPut, headers, queryParams, data)
 }
 
 // Patch performs a HTTP PATCH request. It takes in a URL, user specified headers, query params, data and returns
 // Response and error if exist
 func (c *Client) Patch(url string, headers, queryParams map[string][]string, data interface{}) (response *http.Response, err error) {
 
-	return c.makeRequest(url, http.MethodPatch, headers, nil, queryParams, data)
+	return c.makeRequest(url, http.MethodPatch, headers, queryParams, data)
 }
 
 // Delete performs a HTTP DELETE request. It takes in a URL, user specified headers, query params, data and returns
 // Response and error if exist
 func (c *Client) Delete(url string, headers, queryParams map[string][]string, data interface{}) (response *http.Response, err error) {
 
-	return c.makeRequest(url, http.MethodDelete, headers, nil, queryParams, data)
+	return c.makeRequest(url, http.MethodDelete, headers, queryParams, data)
 }
 
 // Connect performs a HTTP CONNECT request. It takes in a URL, user specified headers, proxyHeaders, query params and returns
 // Response error if exist
-func (c *Client) Connect(url string, headers, proxyHeaders, queryParams map[string][]string) (response *http.Response, err error) {
+func (c *Client) Connect(url string, headers, queryParams map[string][]string) (response *http.Response, err error) {
 
-	return c.makeRequest(url, http.MethodConnect, headers, proxyHeaders, queryParams, nil)
+	return c.makeRequest(url, http.MethodConnect, headers, queryParams, nil)
 }
 
 // Options performs a HTTP Options request. It takes in a URL, user specified headers, query params and returns
 // Response and error if exist
 func (c *Client) Options(url string, headers, queryParams map[string][]string) (response *http.Response, err error) {
 
-	return c.makeRequest(url, http.MethodOptions, headers, nil, queryParams, nil)
+	return c.makeRequest(url, http.MethodOptions, headers, queryParams, nil)
 }
 
 // Trace performs a HTTP TRACE request. It takes in a URL, user specified headers, query params and returns Response
 // and error if exist
 func (c *Client) Trace(url string, headers, queryParams map[string][]string) (response *http.Response, err error) {
 
-	return c.makeRequest(url, http.MethodTrace, headers, nil, queryParams, nil)
+	return c.makeRequest(url, http.MethodTrace, headers, queryParams, nil)
 }
 
 // Custom performs a HTTP request with custom method that the server accepts. It takes in a URL, custom method,
 // user specified headers, query params and returns Response and error if exist
 func (c *Client) Custom(url, method string, headers, queryParams map[string][]string, data interface{}) (response *http.Response, err error) {
 
-	return c.makeRequest(url, method, headers, queryParams, nil, data)
+	return c.makeRequest(url, method, headers, queryParams, data)
 }
 
 // makeRequest is a helper method for the above HTTP methods
-func (c *Client) makeRequest(url, method string, headers, proxyHeaders, queryParams map[string][]string, data interface{}) (response *http.Response, err error) {
+func (c *Client) makeRequest(url, method string, headers, queryParams map[string][]string, data interface{}) (response *http.Response, err error) {
+	c.populateTransport()
 
-	return response, nil
+	// Converting headers to canonical headers
+	canonicalHeaders := make(map[string][]string, len(headers))
+	for headerKey, headerValue := range headers {
+		canonicalHeaders[http.CanonicalHeaderKey(headerKey)] = headerValue
+	}
+
+	contentType, ok := canonicalHeaders["Content-Type"]
+	if ok && len(contentType) >= 1{
+		if contentType[0] == "application/json" || strings.Contains(contentType[0], "application/json") {
+			return c.makeJSONRequest(url, method, headers, queryParams, data)
+		}
+	}
+
+	return c.makeJSONRequest(url, method, headers, queryParams, data)
+}
+
+func (c *Client) makeJSONRequest(url, method string, headers, queryParams map[string][]string, data interface{}) (response *http.Response, err error) {
+	var dataBytes []byte
+	dataBytes, err = json.Marshal(data)
+	if err != nil {
+		return response, err
+	}
+
+	httpClient := http.Client{
+		Transport:     c.transport,
+		Timeout:       c.Timeout,
+	}
+
+	var request *http.Request
+
+	if len(dataBytes) > 0 {
+		request, err = http.NewRequest(method, url, bytes.NewBuffer(dataBytes))
+	} else {
+		request, err = http.NewRequest(method, url, nil)
+	}
+	if err != nil {
+		return response, err
+	}
+
+	q := request.URL.Query()
+
+	for queryKey, queryValues := range queryParams {
+		for _, val := range queryValues {
+			q.Add(queryKey, val)
+		}
+	}
+
+	request.URL.RawQuery = q.Encode()
+
+	for headerKey, headerValues := range headers {
+		for _, val := range headerValues {
+			request.Header.Add(headerKey, val)
+		}
+	}
+
+	return httpClient.Do(request)
+}
+
+// populateTransport populates the transport config
+func (c *Client) populateTransport() {
+	c.transport.DisableKeepAlives = c.DisableKeepAlives
+	c.transport.MaxConnsPerHost = c.MaxConnectionsPerHost
+	c.transport.IdleConnTimeout = c.IdleConnectionTimeout
+	c.transport.MaxConnsPerHost = c.MaxConnectionsPerHost
+	c.transport.MaxIdleConnsPerHost = c.MaxIdleConnectionsPerHost
+	c.transport.MaxIdleConns = c.MaxIdleConnections
+	c.transport.TLSClientConfig = c.TLSClientConfig
 }
